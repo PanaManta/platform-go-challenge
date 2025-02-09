8eaee9cf7e92cdd40f3d5e02bd482b7c85a1a38e
Panagiotis Mantafounis <panagiotismantafounis@gmail.com>
2025-02-09 20:21:26 +0200

    add newline + remove comments

--- tools.go
+++ tools.go
@@ -4,7 +4,7 @@
 package tools
 
 import (
-	_ "github.com/swaggo/swag/cmd/swag" // Swagger generator tool
-	_ "github.com/urfave/cli/v2"        // Dependency for swag tool
-	_ "sigs.k8s.io/yaml"                // Dependency for swag tool
+	_ "github.com/swaggo/swag/cmd/swag"
+	_ "github.com/urfave/cli/v2"
+	_ "sigs.k8s.io/yaml"
 )
